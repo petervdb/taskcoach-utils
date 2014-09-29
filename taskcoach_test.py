3cc1fb93f3dff0c369478fcc20781187293e3a2d
Peter Vandenberghe <petervanden@gmail.com>
2014-09-29 13:43:36 +0200

    Updated taskcoach_test.py

--- taskcoach_test.py
+++ taskcoach_test.py
@@ -3,15 +3,26 @@
 from sys import argv
 from os.path import exists
 import xml.etree.ElementTree as etree
+import getopt
 
 # This is a test program to analyze taskcoach files
+print("This is a small program to analyse your Taskcoach file")
 
-script, taskfile = argv
+# The script requires the Taskcoach file as argument
+# Quit if usage is not good
+try:
+	script, taskfile = argv
+except getopt.GetoptError:
+	print("Usage: %s <toachcoach_file.tsk>" % script)
+	exit(2)
+
+# Quit if file does not exist
 if exists(taskfile):
 	tk_file = etree.parse(taskfile)
 else:
 	print("Sorry, %s does not exist." % taskfile)
 	exit(1)
+
 root = tk_file.getroot()
 print("Document root: %s " % root)
 print("Child elements of root: %s " % len(root))
