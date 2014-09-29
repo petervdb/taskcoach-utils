b4358ff849ee330f146a5818dcb6061f3d097aad
Peter Vandenberghe <petervanden@gmail.com>
2014-09-29 18:32:57 +0200

    First analysis script taskcoach_test.py works

--- taskcoach_test.py
+++ taskcoach_test.py
@@ -4,6 +4,7 @@ from sys import argv
 from os.path import exists
 import xml.etree.ElementTree as etree
 import getopt
+import json
 
 # This is a test program to analyze taskcoach files
 print("This is a small program to analyse your Taskcoach file")
@@ -30,5 +31,25 @@ count = 0
 for child in root:
 	print(child)
 	print(root[count].attrib)
+	cur_text = root[count].attrib
+	cur_tag = root[count].tag
+	# only works for tasks and categories
+	if cur_tag == "task":
+		subject = cur_text['subject']
+		print("Task : %s " % subject)
+	if cur_tag == "category":
+		subject = cur_text['subject']
+		print("Category : %s " % subject)
 	print("-----------------------------------------------------------------------------------")
 	count = count + 1
+# find all tasks
+alltasks =  root.findall('task')
+print ("Let's display all tasks:")
+print(alltasks)
+print("-----------------------------------------------------------------------------------")
+
+# find all categories
+allcategories = root.findall('category')
+print ("Let's display all categories:")
+print(allcategories)
+print("-----------------------------------------------------------------------------------")
