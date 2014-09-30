2744405ddeeda6cb37092e1d1dd481f7dcbd22da
Peter Vandenberghe <petervanden@gmail.com>
2014-09-30 11:46:37 +0200

    Update taskcoach_2_mongodb.py

--- taskcoach_2_mongodb.py
+++ taskcoach_2_mongodb.py
@@ -1,5 +1,19 @@
 #!/usr/bin/python3
 
+# Program              : taskcoach_2_mongodb.py
+# Version              : 0.1.0
+# Created by           : petervdb
+# Last updated by      : petervdb
+# Creation date        : 29/09/2014
+# Last updated         : 30/09/2014
+#
+# You can use this script to analyse a Taskcoach tsk file and insert the content into a MongoDB database
+# MongoDB database used: taskcoach_test
+# MongoDB collections: tasks, categories
+#
+# ToDo
+#
+
 from sys import argv
 from os.path import exists
 import xml.etree.ElementTree as etree
@@ -9,6 +23,7 @@ import pymongo
 con = pymongo.Connection()
 db = con.taskcoach_test
 tasks = db.tasks
+categories = db.categories
 
 # This is a test program to analyze taskcoach files
 print("This is a small program to analyse your Taskcoach file")
@@ -35,7 +50,11 @@ count = 0
 for child in root:
 	print(child)
 	print(root[count].attrib)
+	
+	# cur_text is a JSON which can be used to insert this information into MongoDB
 	cur_text = root[count].attrib
+	
+	# I need the tag to determine where to use the JSON
 	cur_tag = root[count].tag
 	# only works for tasks and categories
 	if cur_tag == "task":
@@ -45,6 +64,7 @@ for child in root:
 	if cur_tag == "category":
 		subject = cur_text['subject']
 		print("Category : %s " % subject)
+		categories.insert(cur_text)
 	print("-----------------------------------------------------------------------------------")
 	count = count + 1
 # find all tasks
